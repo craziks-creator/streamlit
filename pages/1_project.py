20640a6378c53b9c231ea0d793c30e251f059875
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-07 14:48:22 +0530

    Update 1_project.py

--- pages/1_project.py
+++ pages/1_project.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
-
+from PIL import Image
 st.title("Projects")
 
+image = Image.open('sunrise.jpg')
+st.image(image, caption='Sunrise by the mountains')
 st.write("You have entered")
