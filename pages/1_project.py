5251805ae878640d7dc3423fdac2b7e1ed1c9349
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-07 15:01:14 +0530

    Update 1_project.py

--- pages/1_project.py
+++ pages/1_project.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from PIL import Image
 st.title("Projects")
-st.image(https://images.indianexpress.com/2022/11/Telegram-bots-2-1.jpg, caption='Sunrise by the mountains')
+st.image("https://images.indianexpress.com/2022/11/Telegram-bots-2-1.jpg", caption='Sunrise by the mountains')
 st.write("You have entered")
