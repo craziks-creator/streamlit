2f07bc150441ee8de2ea6135629a019d75a2399f
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-07 13:17:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api
-
+'''
 app = Flask(__name__)
 api = Api(app)
 
@@ -11,3 +11,17 @@ class Greeting (Resource):
 
 api.add_resource(Greeting, '/')
 app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
+'''
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+    
+@app.route("/py")
+def salvador():
+    return "Hello, Salvador"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
