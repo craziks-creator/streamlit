76d93f2c8b18e45fb208ae0e546d3a21e6048192
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-07 14:58:09 +0530

    Update 1_project.py

--- pages/1_project.py
+++ pages/1_project.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from PIL import Image
 st.title("Projects")
-
-image = Image.open('https://pixabay.com/vectors/telegram-app-logo-icon-application-5662082/')
-st.image(image, caption='Sunrise by the mountains')
+st.image(https://pixabay.com/vectors/telegram-app-logo-icon-application-5662082/, caption='Sunrise by the mountains')
 st.write("You have entered")
