624c11b0c14b09debaae9b7e60b05a7849bcfd50
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-07 15:11:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from flask_restful import Resource, Api
 import streamlit as st
 st.title("Main Page")
 st.image("https://images.indianexpress.com/2022/11/Telegram-bots-2-1.jpg", caption='this is a telegram feed')
-st.write("this python page is hosted by streamlit ")
+st.subheader("this python page is hosted by streamlit ")
 st.sidebar.success("Select a page above.")
 
