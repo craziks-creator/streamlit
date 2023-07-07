9d3318ec3c2c0ff85992b9560f9c047d0a156acc
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-07 15:04:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,30 +2,6 @@ import os
 from flask import Flask, render_template
 from flask_restful import Resource, Api
 import streamlit as st
-'''
-app = Flask(__name__)
-api = Api(app)
-
-class Greeting (Resource):
-    def get(self):
-        return "Clever Cloud is Up & Running!"
-
-api.add_resource(Greeting, '/')
-app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
-'''
-
 st.title("Main Page")
 st.sidebar.success("Select a page above.")
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello, World!"
-    
-@app.route("/py")
-def py():
-    return "Hello, Salvador"
-    
-if __name__ == "__main__":
-    app.run()
