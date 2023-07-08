f3785dad19797f6f5c28641a611fdb56cf53298b
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-09 01:49:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template
 from flask_restful import Resource, Api
+'''
 import streamlit as st
 st.title("Main Page")
 st.image("https://res.cloudinary.com/practicaldev/image/fetch/s--0cij5eUa--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/t0jgef3vyjid17z8sf3m.png", caption='this is a telegram feed')
@@ -11,6 +12,7 @@ st.text('🌯 Kale, Spinach & Rocket Smoothie')
 st.text('🥚 Hard-Boiled Free-Range Egg')
 st.text('🥝🧉 Avacado Toast')
 st.sidebar.success("Select a page ")
+'''
 app = Flask(__name__)
 
 @app.route("/")
