edc94b0efaabc35592477bce6331166bb2dc49e3
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-09 12:29:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import Flask, render_template
 from flask_restful import Resource, Api
 
 import streamlit as st
+# Using the requests library:
+import requests
+
+try:
+    requests.get("https://hc-ping.com/rMYcF-5DziyIfqUkNWHd2w/streamlit", timeout=10)
+except requests.RequestException as e:
+    # Log ping failure here...
+    print("Ping failed: %s" % e)
 
 st.title("Main Page")
 st.image("https://res.cloudinary.com/practicaldev/image/fetch/s--0cij5eUa--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/t0jgef3vyjid17z8sf3m.png", caption='this is a telegram feed')
