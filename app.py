ae09dbfb31a26c0df73a749240aae2a03d4d8878
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-09 10:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 from flask import Flask, render_template
 from flask_restful import Resource, Api
-'''
+
 import streamlit as st
+'''
 st.title("Main Page")
 st.image("https://res.cloudinary.com/practicaldev/image/fetch/s--0cij5eUa--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/t0jgef3vyjid17z8sf3m.png", caption='this is a telegram feed')
 st.header("this python page is hosted by streamlit ")
@@ -21,9 +22,9 @@ def home():
     return render_template('home.html')
 
 @app.route("/py")
-def py():
-    return "Hello, Salvador"
-
+def streamlit():
+    st.set_page_config(page_title="My Streamlit App")
+    st.write("Hello, world!")
 if __name__ == "__main__":
     app.run()
 
