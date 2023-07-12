3f59651e26063d38558afe2958bdb3e202cfbf7f
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-12 12:27:29 +0530

    Update js.py

--- pages/js.py
+++ pages/js.py
@@ -1,16 +1,10 @@
 from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup
-my_url="https://ssc.nic.in/"
+my_url="https://sscnrs.streamlit.app/~/+/"
 response = requests.get(my_url)
 soup = BeautifulSoup(response.text)
 soup.find(id="error_message")
 # Result:
-'Yay! Supports javascript'
+'Yay! don't support javascript'
 
-driver = webdriver.Chrome()
-driver.get(my_url)
-p_element = driver.find_element_by_id(id_='error_message')
-print(p_element.text)
-# result:
-'Yay! Supports javascript'
