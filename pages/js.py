266963b539e73c56e96f8b5332387af1b50ae81e
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-12 12:43:18 +0530

    Update js.py

--- pages/js.py
+++ pages/js.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 my_url="https://sscnrs.streamlit.app/~/+/"
 response = requests.get(my_url)
 html = requests.get(my_url)
-soup = BeautifulSoup( ..., "html.parser" )
+soup = BeautifulSoup(html, "html.parser" )
 soup.find(id="error_message")
 # Result:
 print(my_url)
