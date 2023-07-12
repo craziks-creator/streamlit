1e419c26f9b3b77c68988681d263eb4357f0f560
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-12 14:40:29 +0530

    Update render.py

--- pages/render.py
+++ pages/render.py
@@ -8,13 +8,13 @@ driver = webdriver.Chrome()
 # URL of website
 url = "https://sscnrs.streamlit.app/~/+/"
 while True: 
-try: 
-# Opening the website
-driver.get(url)
-
-# Getting current URL source code
-get_title = driver.title
-
-# Printing the title of this URL
-print(get_title)
-time.sleep(30)
+  try: 
+  # Opening the website
+  driver.get(url)
+  
+  # Getting current URL source code
+  get_title = driver.title
+  
+  # Printing the title of this URL
+  print(get_title)
+  time.sleep(30)
