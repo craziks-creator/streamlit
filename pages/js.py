3233b30236498b5d0058916ff9a33640a9035ce5
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-12 12:09:02 +0530

    Update js.py

--- pages/js.py
+++ pages/js.py
@@ -1,6 +1,14 @@
 from selenium import webdriver
-driver = webdriver.PhantomJS()
+import requests
+from bs4 import BeautifulSoup
 my_url="https://ssc.nic.in/"
+response = requests.get(my_url)
+soup = BeautifulSoup(response.text)
+soup.find(id="error_message")
+# Result:
+<p id="intro-text">No javascript support</p>
+driver = webdriver.PhantomJS()
+
 driver.get(my_url)
 p_element = driver.find_element_by_id(id_='error_message')
 print(p_element.text)
